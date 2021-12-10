2b555db6265207b9720997a5717511470e3d7880
Petre <BPetre@luxoft.com>
2021-12-10 18:51:28 +0200

    Refatoring the code

--- Python scripts/textbox.py
+++ Python scripts/textbox.py
@@ -1,46 +1,54 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from builtins import staticmethod
 
 
 class TextBox:
     
+    driver = None
     
-    def test_boxes(self, input_dict):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.get("https://demoqa.com/text-box")
-        
-        # input_dict = {'userName': 'test', 'userEmail': 'test@test.com',
-        #               'currentAddress': 'addresstest',
-        #               'permanentAddress': 'permanentaddress'}
-        
-        for key in input_dict:
-            input_text = driver.find_element(By.ID, key)
-            input_text.send_keys(input_dict[key])
-        
-        
-        wait = WebDriverWait(driver, 10)
+    
+    def navigate_to(self, url):
+        TextBox.driver = webdriver.Chrome()
+        TextBox.driver.maximize_window()
+        TextBox.driver.get(url)
+    
+    def get_labels(self):
+        list_of_labels = []
+        elements = TextBox.driver.find_elements(By.XPATH, '//div/form//div//label')
+        for element in elements:
+            list_of_labels.append(element.text)
+        return list_of_labels
+    
+    def input_data(self, input_dict):
+        list_of_labels = self.get_labels()
+        input_xpath = "//div[div/label[text()='{}']]//*[contains(@class, \"form-control\")]"
+        for label in list_of_labels:
+            input_text = TextBox.driver.find_element(By.XPATH,input_xpath.format(label))
+            try:
+                input_text.send_keys(input_dict[label])
+            except KeyError:
+                print("Input label not found")
+        wait = WebDriverWait(TextBox.driver, 10)
         send = wait.until(ec.element_to_be_clickable((By.ID, 'submit')))
+        TextBox.driver.execute_script("window.scrollBy(0,100)", send)
         send.click()
         
-        outputs = driver.find_elements(By.CLASS_NAME, 'mb-1')
+    def verify_output(self, input_dict):
+        outputs = TextBox.driver.find_elements(By.XPATH, "//div[@id='output']//p")
         output_dict = {}
         for output in outputs:
             if output.text.count('Name') == 1:
-                output_dict['userName'] = output.text[output.text.find(":") + 1:]
+                output_dict['Full Name'] = output.text[output.text.find(":") + 1:]
             elif output.text.count('Email') == 1:
-                output_dict['userEmail'] = output.text[output.text.find(":") + 1:]
+                output_dict['Email'] = output.text[output.text.find(":") + 1:]
             elif output.text.count('Current Address ') == 1:
-                output_dict['currentAddress'] = output.text[output.text.find(":") + 1:]
+                output_dict['Current Address'] = output.text[output.text.find(":") + 1:]
             elif output.text.count('Permananet Address ') == 1:
-                output_dict['permanentAddress'] = output.text[output.text.find(":") + 1:]
-        
+                output_dict['Permanent Address'] = output.text[output.text.find(":") + 1:]
+    
         for key in input_dict:
             assert input_dict[key] == output_dict[key]
-        
-        time.sleep(1)
-        
-        driver.quit()
+        TextBox.driver.quit()
--- Tests/box_test.robot
+++ Tests/box_test.robot
@@ -5,8 +5,11 @@ Library    textbox.TextBox
 
 Testing boxes
     ${input_dict}=    Create Dictionary
-    ...    userName=test
-    ...    userEmail=test@test.com
-    ...    currentAddress=address test 3
-    ...    permanentAddress=permanet address test 2
-    Test Boxes    ${input_dict}    
+    ...    Full Name=test
+    ...    Email=test@test.com
+    ...    Current Address=address test 3
+    ...    Permanent Address=permanet address test 2
+    Navigate To    https://demoqa.com/text-box 
+    Input Data    ${input_dict}
+    Verify Output    ${input_dict}
+    
